9f2855dc5e7d59f5e9656e5e82d7b06c3c7ee841
Andre Luiz Silva de Castro <andre-si2011@hotmail.com>
2022-09-23 16:56:16 -0400

    produto service funcionando

--- teste/src/main/java/com/teste/teste/services/ProdutoService.java
+++ teste/src/main/java/com/teste/teste/services/ProdutoService.java
@@ -1,8 +1,37 @@
 package com.teste.teste.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.teste.teste.model.Produto;
+import com.teste.teste.repository.RepositorioProduto;
 @Service
 public class ProdutoService 
 {
+     @Autowired
+     private RepositorioProduto RepositorioProduto;
 
+     public List<Produto> ObterTodos()
+     {
+          return RepositorioProduto.ObterTodos();
+     }
+
+     public Produto adicionar (Produto produto)
+     {
+          return RepositorioProduto.adicionar(produto);
+     }
+
+     public void deletar(Integer id)
+     {
+          RepositorioProduto.deletar(id);
+     }
+
+     public Produto atualizar(Integer id,Produto produto)
+     {
+          produto.setId(id);
+          return RepositorioProduto.atualizar(produto);
+        
+     }
 }
